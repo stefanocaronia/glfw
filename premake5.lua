7080d02ae9cd3a7b1cdcf8afa4f0d606fa7d8904
Stefano Caronia <stefano.caronia@gmail.com>
2019-05-01 23:28:23 +0200

    fixato premake5.lua

--- premake5.lua
+++ premake5.lua
@@ -1,9 +1,14 @@
 project "GLFW"
     kind "StaticLib"
-    language "C"
-    
+    language "c"
+    toolset "gcc"
+
 	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
-    objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+    objdir ("obj/" .. outputdir .. "/%{prj.name}")
+
+    makesettings [[
+        CC = gcc
+    ]]
 
 	files
 	{
@@ -17,12 +22,13 @@ project "GLFW"
         "src/vulkan.c",
         "src/window.c"
     }
-    
+
 	filter "system:windows"
         buildoptions { "-std=c11", "-lgdi32" }
-        systemversion "10.0.17134.0"
-        staticruntime "On"
-        
+        systemversion "latest"
+        staticruntime "on"
+        toolset "gcc"
+
         files
         {
             "src/win32_init.c",
@@ -36,10 +42,11 @@ project "GLFW"
             "src/osmesa_context.c"
         }
 
-		defines 
-		{ 
+		defines
+		{
             "_GLFW_WIN32",
             "_CRT_SECURE_NO_WARNINGS"
-		}
+        }
+
     filter { "system:windows", "configurations:Release" }
         buildoptions "/MT"
